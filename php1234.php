fec5f039a867d546bb23a1bc097132783f9b7941
liquxan <asd0931960112@gmail.com>
2018-05-26 16:50:49 +0800

    6305045

---
+++ php1234.php
@@ -0,0 +1,11 @@
+
+<html>
+   <body>
+   <h2>表單資料:</h2>
+   <form action="hello123.php" method="POST">
+        <input type="text" name="account" value="" placeholder="帳號">
+	    <input type="password" name="password" value="" placeholder="密碼">
+	    <input type="submit">
+    </form>
+	</body>
+	
